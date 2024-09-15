3e60fe6e2149b56907ed15f5e7b8e8a47e9a51d8
pmsavano <pavan.m.savanoor@intel.com>
2024-09-15 16:26:44 +0530

    Update model_config.py
    Fixed the hpu_device_name variable error on GPU code changes

--- dbgpt/configs/model_config.py
+++ dbgpt/configs/model_config.py
@@ -28,32 +28,15 @@ def get_device() -> str:
         import habana_frameworks.torch.core as htcore
         hpu_device_name = torch.device("hpu")
         print("get_device------------", "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "hpu" if hpu_device_name else "cpu")
-        #print("get_device------------", "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
-        #return (
-        #     "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-        #)
         return (
             "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "hpu" if hpu_device_name else "cpu"
         )
     except ModuleNotFoundError:
         import torch
-        print("get_device------------", "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "hpu" if hpu_device_name else "cpu")
-        #print("get_device------------", "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
-        #return (
-        #     "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-        #)
+        print("get_device------------", "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")       
         return (
-            "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "hpu" if hpu_device_name else "cpu"
+            "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
         )
-#        return (
-#            "cuda"
-#            if torch.cuda.is_available()
-#            else "mps"
-#            if torch.backends.mps.is_available()
-#            else "cpu"
-#        )
-#    except ModuleNotFoundError:
-#        return "cpu"
 
 
 LLM_MODEL_CONFIG = {
