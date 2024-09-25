cef9690f876c503ccdcb82889abf6f483c548a75
Rahul Jaikumar <119842802+RahulJaikumar@users.noreply.github.com>
2024-09-25 09:19:51 +0530

    Update tugraph_store.py

--- dbgpt/storage/graph_store/tugraph_store.py
+++ dbgpt/storage/graph_store/tugraph_store.py
@@ -147,12 +147,12 @@ class TuGraphStore(GraphStoreBase):
                 create_vertex_gql = (
                     f"CALL db.createLabel("
                     f"'vertex', '{self._vertex_type}', "
-                    f"'id', ['id',string,false],"
-                    f"['name',string,false],"
-                    f"['_document_id',string,true],"
-                    f"['_chunk_id',string,true],"
-                    f"['_community_id',string,true],"
-                    f"['description',string,true])"
+                    f"'id', ['id','string',false],"
+                    f"['name','string',false],"
+                    f"['_document_id','string',true],"
+                    f"['_chunk_id','string',true],"
+                    f"['_community_id','string',true],"
+                    f"['description','string',true])"
                 )
                 self.conn.run(create_vertex_gql)
                 self._add_vertex_index("_community_id")
@@ -160,8 +160,8 @@ class TuGraphStore(GraphStoreBase):
                 create_vertex_gql = (
                     f"CALL db.createLabel("
                     f"'vertex', '{self._vertex_type}', "
-                    f"'id', ['id',string,false],"
-                    f"['name',string,false])"
+                    f"'id', ['id','string',false],"
+                    f"['name','string',false])"
                 )
                 self.conn.run(create_vertex_gql)
 
@@ -170,16 +170,16 @@ class TuGraphStore(GraphStoreBase):
                     'edge', '{self._edge_type}',
                     '[["{self._vertex_type}",
                     "{self._vertex_type}"]]',
-                    ["id",STRING,false],
-                    ["name",STRING,false])"""
+                    ["id",'string',false],
+                    ["name",'string',false])"""
             if self._summary_enabled:
                 create_edge_gql = f"""CALL db.createLabel(
                     'edge', '{self._edge_type}',
                     '[["{self._vertex_type}",
                     "{self._vertex_type}"]]',
-                    ["id",STRING,false],
-                    ["name",STRING,false],
-                    ["description",STRING,true])"""
+                    ["id",'string',false],
+                    ["name",'string',false],
+                    ["description",'string',true])"""
             self.conn.run(create_edge_gql)
 
     def _format_query_data(self, data, white_prop_list: List[str]):
